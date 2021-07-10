4043dce161bd47b4007038bd9ff02bf439a54328
abhishek dahal <57648908+abhishekdahal72@users.noreply.github.com>
2021-07-10 20:58:13 +0545

    User profile

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom';
 import SignUp from './SignUp';
+import UserProfile from './UserProfile';
 class Body extends Component {
     render() {
         return (
-            <div>
-                <Route path="/signup" exact component={SignUp} />
-                </div>
-        )
+          <div>
+            <Route path='/signup' exact component={SignUp} />
+            <Route path='/profile' exact component={UserProfile} />
+          </div>
+        );
     }
 }
 export default Body;
