2f185e2e4fd0180f4aa8f6200c5099bbe475c388
MandeepM <maharjanmemandeep@gmail.com>
2021-07-12 22:56:24 +0545

    restructure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter } from "react-router-dom";
 import "./css/style.css";
-import Body from "./Body/Body";
-import Login from "./Body/LoginIn";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { Route } from "react-router-dom";
+import LogIn from "./pages/Login";
+import SignUp from "./pages/Signup";
+import UserProfile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h1>Hello app!</h1>
-        <Body />
-        <Login />
+        <Route path="/login" exact component={LogIn} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/profile" exact component={UserProfile} />
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/Profile.js
@@ -0,0 +1,98 @@
+import React, { Component, state } from "react";
+import { Col, Container, Row, Card, Table } from "react-bootstrap";
+import axios from "axios";
+import userprofile from "../media/user.png";
+
+class UserProfile extends Component {
+  state = {
+    id: localStorage.getItem("userId"),
+    fname: "",
+    lname: "",
+    username: "",
+    address: "",
+    mobile: "",
+    email: "",
+    profilepic: "",
+    config: {
+      headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
+    },
+    user: JSON.parse(localStorage.getItem("user")) || [],
+  };
+
+  componentDidMount() {
+    const userName = this.state.user.username;
+    axios
+      .get(`http://localhost:5000/user/${userName}`)
+      .then((response) => {
+        this.setState({
+          fname: response.data.fname,
+          lname: response.data.lname,
+          username: response.data.username,
+          email: response.data.email,
+          profilepic: response.data.profilepic,
+          mobile: response.data.mobile,
+          address: response.data.address,
+        });
+      })
+      .catch((err) => {
+        console.log("PROF ERROR", err);
+      });
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md={4} sm={4} xs={12}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={userprofile} />
+            </Card>
+          </Col>
+          <Col md={8} sm={4} xs={12}>
+            <div className="user-details">
+              <ul>
+                <li>
+                  <p>{this.state.username}</p>
+                </li>
+                <li>
+                  <h3>
+                    {this.state.fname} {this.state.lname}
+                  </h3>
+                </li>
+                <li>
+                  <p>{this.state.lname}</p>
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="user-updated-details">
+            <Table striped bordered hover variant="dark">
+              {/* <thead>
+                <tr>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                </tr>
+              </thead> */}
+              <tbody>
+                <tr>
+                  <td>Address</td>
+                  <td>{this.state.username}</td>
+                </tr>
+                <tr>
+                  <td>Mobile</td>
+                  <td>{this.state.mobile}</td>
+                </tr>
+                <tr>
+                  <td>E-mail</td>
+                  <td>{this.state.email}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+export default UserProfile;
