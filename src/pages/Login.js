99b6837bac0bb26dcc9ae88826c4d22ce49086dc
ManojKC123 <57611035+ManojKC123@users.noreply.github.com>
2021-07-29 16:07:24 +0545

    login notification

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,48 @@
-import React, { Component, state, getUserData } from "react";
+import React, { Component } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure();
 
 class LoginIn extends Component {
-  state = {
-    username: "",
-    password: "",
-    token: "",
-    isLoggedIn: "",
-    message: "",
-  };
-
-  getUserData = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isLoggedIn: "false",
+      message: "",
+    };
+  }
+  notify = () => {
+    toast.error(this.state.message, {
+      position: toast.POSITION.BOTTOM_RIGHT,
     });
   };
 
   loginhandler = (e) => {
     e.preventDefault();
+    const data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
     axios
-      .post("https://student-verse.herokuapp.com/login", this.state)
+      .post("https://student-verse.herokuapp.com/login", data)
       .then((response) => {
-        console.log(response);
+        console.log("login response", response);
         var user = {
           username: this.state.username,
           token: response.data.token,
           isLoggedIn: true,
         };
+        this.setState({
+          message: response.data.message,
+        });
+        console.log(response.data.userID);
         localStorage.setItem("user", JSON.stringify(user));
         localStorage.setItem("token", response.data.token);
+        window.location.href = "/";
       })
       .catch((err) => {
         console.log(err);
@@ -38,12 +52,8 @@ class LoginIn extends Component {
       });
   };
   render() {
-    if (this.state.message) {
-      var message = this.state.message;
-    }
-
     if (this.state.isLoggedIn === true) {
-      return (window.location.href = "/profile");
+      return (window.location.href = "/");
     }
     return (
       <div className="container register">
@@ -64,51 +74,54 @@ class LoginIn extends Component {
                 <h3 className="register-heading">
                   <b>Login</b>
                 </h3>
-                <div className="row register-form">
-                  <p>{message}</p>
-                  <div className="col-md-6 login-form">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="username"
-                        value={this.state.username}
-                        onChange={(event) => {
-                          this.setState({
-                            username: event.target.value,
-                          });
-                        }}
-                        placeholder="Username"
+                <div className="row">
+                  <form onSubmit={this.loginhandler}>
+                    <div className="col-md-6 login-form">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="username"
+                          value={this.state.username}
+                          onChange={(event) => {
+                            this.setState({
+                              username: event.target.value,
+                            });
+                          }}
+                          placeholder="Username"
+                          required=""
+                        />
+                      </div>
+
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="password"
+                          value={this.state.password}
+                          onChange={(event) => {
+                            this.setState({
+                              password: event.target.value,
+                            });
+                          }}
+                          placeholder="Password"
+                          required=""
+                        />
+                      </div>
+
+                      <button
+                        type="submit"
+                        value="LOGIN"
+                        onClick={this.notify}
                       />
+
+                      <div className="d-flex justify-content-center links">
+                        <a href="/signup">
+                          If you don't have account? <b>Go To SignUp</b>{" "}
+                        </a>
+                      </div>
                     </div>
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        className="form-control"
-                        id="username"
-                        value={this.state.password}
-                        onChange={(event) => {
-                          this.setState({
-                            password: event.target.value,
-                          });
-                        }}
-                        placeholder="Password"
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className="btnRegister"
-                      value="send"
-                      onClick={this.loginhandler}
-                    >
-                      Login
-                    </button>
-                    <div className="d-flex justify-content-center links">
-                      <a href="/signup">
-                        If you don't have account? <b>Go To SignUp</b>{" "}
-                      </a>
-                    </div>
-                  </div>
+                  </form>
                 </div>
               </div>
             </div>
