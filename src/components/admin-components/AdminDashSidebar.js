25fb97aa7ca838eb5732171894dca7a2b0c5023f
MandeepM <maharjanmemandeep@gmail.com>
2021-08-06 01:25:49 +0545

    dashboard-sidebar setup for post-studymaterial

--- src/components/admin-components/AdminDashSidebar.js
+++ src/components/admin-components/AdminDashSidebar.js
@@ -1,24 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Dashboard, MenuBook, Category } from "@material-ui/icons/";
+import {
+  Dashboard,
+  MenuBook,
+  Category,
+  KeyboardArrowDown,
+} from "@material-ui/icons/";
 
-function AdminDashSidebar() {
+const AdminDashSidebar = () => {
+  const [currentSub, setCurrentSub] = useState("");
+  const [subject, setSubject] = useState(["Science", "EPH"]);
+  const [studymaterial, setStudyMaterial] = useState(false);
+
+  const createSubject = (e) => {
+    e.preventDefault();
+    setSubject([...subject, currentSub]);
+    setCurrentSub("");
+  };
+  const toggleGroup = (e) => {
+    setStudyMaterial(!studymaterial);
+    console.log(studymaterial);
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar-inner">
-        <ul>
+        <ul className="sidebar-list">
           <Link to="/admin">
             <li>
               <Dashboard className="sidebar-icons" />
               Dashboard
             </li>
           </Link>
-          <Link to="/admin/studymaterials">
-            <li>
-              <MenuBook className="sidebar-icons" />
-              Study Materials
-            </li>
-          </Link>
+          <li onClick={() => toggleGroup("study-material")}>
+            <MenuBook className="sidebar-icons" />
+            Study Materials
+            <KeyboardArrowDown
+              className={studymaterial ? "drop-icon toggle" : "drop-icon"}
+            />
+          </li>
+          <ul
+            className={
+              !studymaterial ? "subject-lists toggle" : "subject-lists"
+            }
+            id="study-material"
+          >
+            {subject &&
+              subject.map((sub, index) => {
+                return (
+                  <Link to="/admin">
+                    <div className="subject-name" key={index}>
+                      {sub}
+                    </div>
+                  </Link>
+                );
+              })}
+            <div class="">
+              <input
+                type="text"
+                className="form-control"
+                id="subjectInput"
+                value={currentSub}
+                onChange={(event) => {
+                  setCurrentSub(event.target.value);
+                  console.log("onchange currentsub", currentSub);
+                }}
+                placeholder="New Subject"
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  createSubject(e);
+                }}
+                class="btn btn-primary"
+              >
+                Create subject
+              </button>
+            </div>
+          </ul>
+
           <Link to="/admin/quiz">
             <li>
               <Category className="sidebar-icons" />
@@ -29,6 +89,6 @@ function AdminDashSidebar() {
       </div>
     </div>
   );
-}
+};
 
 export default AdminDashSidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,14 +23,12 @@ const Home = () => {
         <Grid item xs={9} md={9} className="">
           <div className="questions-grid">
             <h3 className="questions-headline">Top Questions</h3>
-            <div className="questions-btn">
-              <Link
-                to={user.token ? "/ask-question" : "/login"}
-                className="btn btn-primary"
-              >
-               <b> Ask Question</b>
-              </Link>
-            </div>
+            <Link
+              to={user.token ? "/ask-question" : "/login"}
+              className="btn btn-primary"
+            >
+              Ask Question
+            </Link>
           </div>
           <div className="questions">
             {posts.map((post) => (
