61bf3d22832d48b3bec1a334c983ddaf8cb4a652
abhishek dahal <57648908+abhishekdahal72@users.noreply.github.com>
2021-08-06 15:33:58 +0545

    add topic

---
+++ src/pages/admin/AddTopics.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { KeyboardArrowDown } from "@material-ui/icons/";
+
+const AddTopics = () => {
+  const [currentTopic, setCurrentTopic] = useState("");
+  const [currentSubTopic, setCurrentSubTopic] = useState("");
+  const [topic, setTopic] = useState(["Physics", "Chemistry"]);
+  const [subtopic, setSubTopic] = useState(["Measurement", "Kinematics"]);
+  const [studytopic, setStudyTopic] = useState(false);
+
+  // const [studysubtopic, setStudySubTopic] = useState(false);
+
+  const createTopic = (e) => {
+    e.preventDefault();
+    setTopic([...topic, currentTopic]);
+    setCurrentTopic("");
+  };
+
+  const createSubTopic = (e) => {
+    e.preventDefault();
+    setSubTopic([...subtopic, currentSubTopic]);
+    setCurrentSubTopic("");
+  };
+
+  const toggleGroup = (e) => {
+    setStudyTopic(!studytopic);
+    console.log(studytopic);
+  };
+
+  return (
+    <div className="topic-section">
+      <div className="container-fluid">
+        <div className="topic-listwrap">
+          <h3 className="topic-title">Topics:</h3>
+          <ul className="topic-lists" id="study-topic">
+            {topic &&
+              topic.map((sub, index) => {
+                return (
+                  <>
+                    <div
+                      className="topic-name"
+                      key={index}
+                      onClick={() => toggleGroup("study-material")}
+                    >
+                      {sub}topics
+                      <KeyboardArrowDown
+                        className={
+                          studytopic ? "drop-icon toggle" : "drop-icon"
+                        }
+                      />
+                    </div>
+                    <div
+                      className={
+                        !studytopic ? "subtopic-lists toggle" : "subtopic-lists"
+                      }
+                      id="subtopic"
+                    >
+                      {subtopic &&
+                        subtopic.map((sub, index) => {
+                          return (
+                            <Link to="/admin/topic/subtopic">
+                              <div className="subtopic-name" key={index}>
+                                {sub}sub
+                              </div>
+                            </Link>
+                          );
+                        })}
+                      <div class="">
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="subTopicInput"
+                          value={currentSubTopic}
+                          onChange={(event) => {
+                            setCurrentSubTopic(event.target.value);
+                            console.log(
+                              "onchange currentsubtopic",
+                              currentSubTopic
+                            );
+                          }}
+                          placeholder="New Sub-Topic"
+                        />
+                        <button
+                          type="button"
+                          onClick={(e) => {
+                            createSubTopic(e);
+                          }}
+                          class="btn btn-primary"
+                        >
+                          Create Sub-Topic
+                        </button>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            <div class="">
+              <input
+                type="text"
+                className="form-control input-topic"
+                id="topicInput"
+                value={currentTopic}
+                onChange={(event) => {
+                  setCurrentTopic(event.target.value);
+                  console.log("onchange currenttopic", currentTopic);
+                }}
+                placeholder="New Topic"
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  createTopic(e);
+                }}
+                class="btn btn-primary"
+              >
+                Create Topic
+              </button>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AddTopics;
