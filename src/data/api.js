bc1f6e1342e168c0d3e70c46e35844ccca0061db
MandeepM <maharjanmemandeep@gmail.com>
2021-08-08 00:59:25 +0545

    api change

--- src/data/api.js
+++ src/data/api.js
@@ -1,7 +1,7 @@
 import { makeRequest } from "./request";
 
-// const apiURL = "https://student-verse.herokuapp.com";
-const apiURL = "http://localhost:5000";
+const apiURL = "https://student-verse.herokuapp.com";
+// const apiURL = "http://localhost:5000";
 const siteURI = `${window.location.origin}/`;
 
 export function login(loginData) {
