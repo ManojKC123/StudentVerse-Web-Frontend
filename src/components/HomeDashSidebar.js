8373fac9964ab6c9f25e8498197efcd3e134801a
MandeepM <maharjanmemandeep@gmail.com>
2021-08-11 20:21:43 +0545

    after rebase usertype_from_dev

---
+++ src/components/HomeDashSidebar.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 0,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 324,
+    position: 'fixed',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className='sidebar-user'>
+      <Tabs
+        orientation='vertical'
+        variant='scrollable'
+        value={value}
+        onChange={handleChange}
+        aria-label='Vertical tabs example'
+        className={classes.tabs}
+      >
+        <Link to='/'>
+          <Tab label='Home' {...a11yProps(0)} />
+        </Link>
+        <Tab label='Tags' {...a11yProps(2)} />
+        <Link to='/user-search'>
+          <Tab label='Users' {...a11yProps(3)} />
+        </Link>
+        <Tab label='Quiz' {...a11yProps(4)} />
+      </Tabs>
+    </div>
+  );
+}
---
+++ src/pages/UserSearch.js
@@ -0,0 +1,14 @@
+import React, { Component, Container } from 'react';
+
+class UserSearch extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="heading-user">Usaaaaaaaaaaaaaaaaaaaaasers</h1>
+                
+        </div>
+        )
+    }
+}
+
+export default UserSearch;
