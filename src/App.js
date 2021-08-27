8d4af4db1cb181cd242c89267c43767566407e21
abhishek dahal <57648908+abhishekdahal72@users.noreply.github.com>
2021-08-27 16:05:16 +0545

    quiz normal

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ import AdminDash from "./pages/admin/AdminDash";
 import UserQuiz from "./pages/UserQuiz";
 import AddTopics from "./pages/admin/AddTopics";
 import TagSearch from "./pages/TagSearch";
-
 function App() {
   return (
     <div className="App">
--- src/components/QuizPanel.js
+++ src/components/QuizPanel.js
@@ -1,59 +1,134 @@
 import React, { useState, useEffect, useRef } from "react";
-import { addQuestion } from "../data/api";
+import {} from "../data/api";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Grid } from "@material-ui/core";
-import { loadQuiz } from "../data/api";
+import { loadQuiz, sendScore } from "../data/api";
 
 toast.configure();
 
 function QuizPanel(props) {
   const [user] = useState(JSON.parse(localStorage.getItem("user")) || []);
-  const [formData, setFormData] = useState({
-    title: "",
-    body: "",
-    tags: [],
-  });
-  const [quizDetails, setQuizDetails] = useState([]);
-  const [randomQuiz, setRandomQuiz] = useState(Array(10).fill(0)) || [];
-
   const [site] = useState(JSON.parse(localStorage.getItem("site")) || []);
+  const [quizDetails, setQuizDetails] = useState([]);
+  let [currentQuiz, setCurrentQuiz] = useState(0);
+  const [quizScore, setQuizscore] = useState(0);
+  const [quizFTime, setQuizFTime] = useState(0);
+  const [minutes, setMinutes] = useState(5);
+  const [seconds, setSeconds] = useState(0);
+  const [startTimer, setStartTimer] = useState(false);
+  let myInterval;
 
-  //   useEffect(() => {}, []);
+  useEffect(() => {
+    if (startTimer) {
+      myInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(myInterval);
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
 
-  //   const onChange = (e) =>
-  //     setFormData({ ...formData, [e.target.name]: e.target.value });
+      return () => {
+        clearInterval(myInterval);
+      };
+    }
+  }, [startTimer, seconds]);
 
-  const onSubmit = async (e) => {
-    // e.preventDefault();
-  };
   const startQuiz = (e) => {
     loadQuiz(site.chapter)
       .then((response) => {
         if (response.success === true && response.data) {
-          setQuizDetails(response.data);
+          const random = response.data.sort(() => 0.5 - Math.random());
+          let selected = random.slice(0, 10);
+          console.log("here is quiz", selected);
+          setQuizDetails(selected);
+          setStartTimer(true);
         }
       })
       .catch((err) => {
         console.log("Add answer error", err.response);
       });
+    console.log("curr quiz", currentQuiz);
+  };
 
-    const random = quizDetails.sort(() => 0.5 - Math.random());
-    let selected = random.slice(0, 10);
-    setRandomQuiz(selected);
+  const handleQuiz = ({ option, answer, type }) => {
+    if (type === "Next") {
+      setCurrentQuiz(currentQuiz + 1);
+    }
+    if (type === "Previous") {
+      setCurrentQuiz(currentQuiz - 1);
+    }
+    if (type === "Submit") {
+      console.log("option", option);
+      console.log("answer", answer);
+      if (option === answer) {
+        console.log("right answer", option, answer);
+        setQuizscore(quizScore + 1);
+      }
+      setCurrentQuiz(currentQuiz + 1);
+      console.log("qdetail", quizDetails);
+    }
+    if (type === "Finish") {
+      clearInterval(myInterval);
+      let finishedTime = { minutes, seconds };
+      let timeTaken = 300 - (finishedTime.minutes * 60 + seconds);
+      let mm = parseInt(timeTaken / 60);
+      let ss = timeTaken % 60;
+      let timeTms = mm + ":" + ss;
+      let ext = mm === 0 ? "secs" : " mins";
+      let quizName = site.chapterName;
+      let scoreMsg =
+        "Acheived " +
+        quizScore +
+        " out of 10 in " +
+        site.chapterName +
+        "'s quiz in " +
+        mm +
+        ":" +
+        ss +
+        ext;
+      let scoreData = { quizScore, timeTms, quizName };
+      // console.log("score", scoreData);
+      sendScore(scoreData, user.token)
+        .then((response) => {
+          if (response.success === true && response.data) {
+            toast.success(scoreMsg, {
+              position: toast.POSITION.BOTTOM_RIGHT,
+            });
+          }
+        })
+        .catch((err) => {
+          console.log("Quiz score error", err.response);
+        });
+    }
   };
 
   return (
     <div className="quiz">
       <Grid container>
         <Grid item md={9}>
-          <button
-            onClick={startQuiz}
-            class="btn btn-primary mb-3 btnsubmit-quiz"
-          >
-            Start quiz
-          </button>
-          <form onSubmit={(e) => onSubmit(e)}>
+          <div className="quiz-start">
+            <button
+              onClick={startQuiz}
+              class="btn btn-primary mb-3 btnsubmit-quiz"
+            >
+              Start quiz
+            </button>
+
+            {minutes === 0 && seconds === 0 ? null : (
+              <h1>
+                {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+              </h1>
+            )}
+          </div>
+          <form>
             <label className="form-label s-label">
               <b>Lets start the quiz</b>
               <p className="title-desc fw-normal fs-caption">
@@ -61,47 +136,60 @@ function QuizPanel(props) {
               </p>
             </label>
 
-            {randomQuiz &&
-              randomQuiz.map((ques, index) => {
-                return (
-                  <div className="give-quiz" key={index}>
-                    <label class="form-label quiz-question">
-                      {ques.question}
-                    </label>
-                    <div className="row">
-                      <div class="form-check quiz-row1">
-                        {ques.options &&
-                          ques.options.map((option, i) => {
-                            return (
-                              <div className="quiz-option">
-                                <input
-                                  class="form-check-input"
-                                  type="checkbox"
-                                  value=""
-                                  id="flexCheckDefault"
-                                />
-                                <label
-                                  class="form-check-label "
-                                  for="flexCheckDefault"
-                                  key={i}
-                                >
-                                  {option}
-                                </label>
-                              </div>
-                            );
-                          })}
+            <div className={`give-quiz`}>
+              <label class="form-label quiz-question">
+                {quizDetails[currentQuiz]?.question}
+              </label>
+              <div class="quiz-opt-wrap">
+                {quizDetails &&
+                  quizDetails[currentQuiz]?.options.map((option, i) => {
+                    return (
+                      <div className="quiz-option">
+                        <input
+                          class=""
+                          type="button"
+                          value={option}
+                          onClick={() =>
+                            handleQuiz({
+                              option: option,
+                              answer: quizDetails[currentQuiz].answer,
+                              type: "Submit",
+                            })
+                          }
+                        />
                       </div>
-                    </div>
-                    <button
-                      type="submit"
-                      class="btn btn-primary mb-3 btnsubmit-quiz"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                );
-              })}
+                    );
+                  })}
+              </div>
+            </div>
           </form>
+          <div className="quiz-button-group">
+            <button
+              class="btn btn-primary btnsubmit-quiz"
+              onClick={() => handleQuiz({ type: "Previous" })}
+            >
+              Previous
+            </button>
+            <button
+              class="btn btn-primary btnsubmit-quiz"
+              onClick={() =>
+                handleQuiz({ currentquiz: currentQuiz, type: "Submit" })
+              }
+            >
+              Submit
+            </button>
+            <button
+              class="btn btn-primary btnnext-quiz"
+              onClick={() =>
+                handleQuiz({
+                  type:
+                    quizDetails.length - 1 > currentQuiz ? "Next" : "Finish",
+                })
+              }
+            >
+              {quizDetails.length - 1 > currentQuiz ? "Next" : "Finish"}
+            </button>
+          </div>
         </Grid>
         <Grid item md={3}>
           <div className="test">kjklkjhlkjh</div>
